c62db94a04cb7463cbc4eac753c803b1de234a7a
MounikaDasa <dasamounika@gmail.com>
2023-07-14 17:20:09 +0530

    used md5

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,7 +19,7 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -40,7 +41,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
 const new_user=User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
 });
 new_user.save()
 res.render("secrets")
@@ -48,7 +49,7 @@ res.render("secrets")
 
 app.post("/login",function(req,res){
     const get_email=req.body.username;
-    const get_pswd=req.body.password;
+    const get_pswd=md5(req.body.password);
     User.findOne({email:get_email}).then(function(details){
         if(details){
             if(details.password===get_pswd){
